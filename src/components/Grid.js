e9d82673a8c9eb50acf21f7b0b7dfff6af559385
James <james_gower@outlook.com>
2018-04-06 15:17:45 +0100

    more responsive styling (bootstrap 4)

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,25 +2,9 @@ import React from 'react';
 import { GridList, GridTile } from 'material-ui/GridList';
 import { Redirect } from 'react-router-dom';
 
-const styles = {
-	root: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		justifyContent: 'space-around',
-		overflow: 'hidden',
-		marginBottom: '80px'
-	},
-	gridList: {
-		minWidth: 'auto',
-		minHeight: '100%',
-		overflow: 'hidden',
-	},
-};
-
 /*
 	TODO //
 	!! Find API key in portfolio and secure it 
-	!! FIX ANOTHER FUCKING BUG WITH THE TIMERS GOING OFF	
 	[ ]	Add loading page
 	[ ] Make different components for Headroom Navbar and normal Navbar
 	[x]	Add correct href references
@@ -29,7 +13,7 @@ const styles = {
 	[x] Add images to grid
 	[x]	Style grid and add animations
 	[ ] Add tags array to tiles object for sorting via tags.
-	[ ] Create different styles for the grid and text based on window size
+	[x] Create different styles for the grid and text based on window size
 	[x] Links in Wikipedia Component not working
 	[x] Fix wrong path for fail.mp3 in Simon Component
 	[x] Add font back to Pomodoro component
@@ -37,70 +21,80 @@ const styles = {
 
 const tilesData = [
 	{
-		img: '/images/pomodoro.jpg',
-		title: 'Pomodoro Clock',
-		subtitle: 'Built with React, Pure JavaScript, jQuery, Babel & CSS',
-		href: '/portfolio/pomodoro',
-	},
-	{
-		img: '/images/blogify.jpg',
-		title: 'Blogify App',
-		subtitle: 'Built with React, Redux, React-Router, Webpack, SASS & Jest',		
-		featured: true,
-		href: '/portfolio/blogify',
-	},
-	{
-		img: '/images/tictactoe.jpg',
-		title: 'Tic-Tac-Toe with Minimax Algorithm',
-		subtitle: 'Built with React, SASS & Babel',		
-		featured: true,
-		href: '/portfolio/tic-tac-toe',
-	},
-	{
-		img: '/images/calculator.jpg',
-		title: 'Calculator',
-		subtitle: 'Built with React 16, SASS & Babel',		
-		href: '/portfolio/calculator',
-	},
-	{
-		img: '/images/wiki.jpg',
-		title: 'Wikipedia API',
-		subtitle: 'Built with jQuery, AJAX and pure JavaScript',		
-		href: '/portfolio/wikipedia',
-	},
-	{
-		img: '/images/expensify.jpg',
-		title: 'Expensify App',
-		subtitle: 'Built in React 16, Redux, React-Router, Webpack, SASS & Jest',		
-		featured: true,
-		href: '/portfolio/expensify',
-	},
-	{
-		img: '/images/twitch.jpg',
-		title: 'Twitch API',
-		subtitle: 'Build with React, Webpack, Twitch\'s API & SCSS',
-		featured: true,
-		href: '/portfolio/twitch'
-	},
-	{
-		img: '/images/indecision.jpg',
-		title: 'Indecision App',
-		subtitle: 'Build with React, Webpack & React-Router',
-		href: '/portfolio/indecision-app'
-	}, 
-	{
 		img: '/images/simon.jpg',
 		title: 'Simon Says',
-		subtitle: 'Build with React, Webpack & React-Router',
+		subtitle: 'React, Webpack, React-Router',
 		href: '/portfolio/simon-says',
+		color: 'yellow',
 	},
 	{
 		img: '/images/emaily.jpg',
 		title: 'Emaily App',
-		subtitle: 'Full Stack App - Built with Express, SendGrid, MongoDB, React, Redux ',
+		subtitle: 'Full Stack App - Express, SendGrid, MongoDB, React, Redux, Stripe',
 		href: '/portfolio/emaily',
 		featured: true,
-	}
+		color: 'red',
+	},
+	{
+		img: '/images/tictactoe.jpg',
+		title: 'Tic-Tac-Toe with Minimax Algorithm',
+		subtitle: 'React, SASS, Pure JS, Babel',
+		featured: true,
+		href: '/portfolio/tic-tac-toe',
+		color: 'green',
+	},
+	{
+		img: '/images/pomodoro.jpg',
+		title: 'Pomodoro Clock',
+		subtitle: 'React, Pure JS, jQuery, Babel',
+		href: '/portfolio/pomodoro',
+		color: '#303030',
+	},
+	{
+		img: '/images/wiki.jpg',
+		title: 'Wikipedia API',
+		subtitle: 'jQuery, AJAX, Pure JS, React',
+		href: '/portfolio/wikipedia',
+		color: 'blue',
+	},
+	{
+		img: '/images/expensify.jpg',
+		title: 'Expensify App',
+		subtitle: 'React, Redux, React-Router, Webpack, SASS, Jest',
+		featured: true,
+		href: '/portfolio/expensify',
+		color: '#364051',
+	},
+	{
+		img: '/images/twitch.jpg',
+		title: 'Twitch API',
+		subtitle: "React, Webpack, XHR, Twitch API & SCSS",
+		featured: true,
+		href: '/portfolio/twitch',
+		color: 'blueviolet',
+	},
+	{
+		img: '/images/indecision.jpg',
+		title: 'Indecision App',
+		subtitle: 'React, Webpack, React-Router, Local Storage',
+		href: '/portfolio/indecision-app',
+		color: 'darkblue',
+	},
+	{
+		img: '/images/calculator.jpg',
+		title: 'Calculator',
+		subtitle: 'React, SASS, Pure JS',
+		href: '/portfolio/calculator',
+		color: 'turquoise',
+	},
+	{
+		img: '/images/blogify.jpg',
+		title: 'Blogify App',
+		subtitle: 'React, Redux, React-Router, Webpack, SASS, Jest',
+		featured: true,
+		href: '/portfolio/blogify',
+		color: 'blue',
+	},
 ];
 
 export class Grid extends React.Component {
@@ -108,7 +102,8 @@ export class Grid extends React.Component {
 		super();
 
 		this.state = {
-			redirect: false
+			redirect: false,
+			desktop: window.innerWidth > 800,
 		};
 	}
 	//Random animation for each element in the grid
@@ -124,43 +119,81 @@ export class Grid extends React.Component {
 		this.animations = setInterval(() => {
 			this.randomAnimation();
 		}, 8000);
-		document.getElementById('grid').className = 'animated fadeIn'; 
+		document.getElementById('grid').className = 'animated fadeIn';
+		this.updateWindowDimensions();
+		window.addEventListener('resize', this.updateWindowDimensions);
 	};
 
 	componentWillUnmount = () => {
 		clearInterval(this.animations);
+		window.removeEventListener('resize', this.updateWindowDimensions);
 	};
 
-	handleOnClick = (route) => {
+	handleOnClick = route => {
 		this.setState({
 			route,
-			redirect: true
+			redirect: true,
 		});
-	}
+	};
+
+	updateWindowDimensions = () => {
+		let desktop = window.innerWidth > 800;
+		this.setState({ desktop });
+	};
 
 	render() {
-		if(this.state.redirect) {
+		if (this.state.redirect) {
 			return <Redirect push to={this.state.route} />;
 		}
+
+		const styles = {
+			root: {
+				display: 'flex',
+				flexWrap: 'wrap',
+				justifyContent: 'space-around',
+				overflow: 'hidden',
+				marginBottom: '80px',
+			},
+			gridList: {
+				minWidth: 'auto',
+				minHeight: '100%',
+				overflow: 'hidden',
+			},
+		};
+
 		return (
 			//GridTile are mapped from the TilesData array so each element is rendered
 			<div className="grid-container" id="grid" style={styles.root}>
-				<GridList cols={3} cellHeight={240} padding={20} style={styles.gridList}>
-					{ tilesData.map((tile, i) => (
+				<GridList
+					cols={this.state.desktop ? 3 : 2}
+					cellHeight={this.state.desktop ? 240 : 140}
+					padding={20}
+					style={styles.gridList}
+				>
+					{tilesData.map((tile, i) => (
 						<GridTile
 							id={`tile${i}`}
 							key={`tile${i}`}
 							className="grid-list"
-							onClick={ () => this.handleOnClick(tile.href) }
+							onClick={() => this.handleOnClick(tile.href)}
 							title={tile.title}
+							titleStyle={{
+								fontSize: this.state.desktop ? '16px' : '13px'
+							}}
 							subtitle={tile.subtitle}
+							subtitleStyle={{
+								fontSize: this.state.desktop ? '13px' : '8px',
+							}}
+							style={{
+								border: `4px solid ${tile.color}`,
+							}}
 							actionPosition="left"
 							titlePosition="bottom"
-							titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
-							cols={tile.featured ? 2 : 1}
+							titleBackground={`linear-gradient(to bottom, ${tile.color} 0%, black 100%)`}
+							cols={this.state.desktop ? (tile.featured ? 2 : 1) : 1}
 							rows={2}
 						>
-							<img src={tile.img} className="grid-list"/>
+							<img src={tile.img} className="grid-list" />
 						</GridTile>
 					))}
 				</GridList>
