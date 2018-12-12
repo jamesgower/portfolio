7aae15b34b1c49cedc7bfddd952af79107f6f7ce
James <james_gower@outlook.com>
2018-12-12 10:50:49 +0000

    changed package.json, updated scripts and updated packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,73 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = env => {
-	const isProduction = env === 'production';
-	const CSSExtract = new ExtractTextPlugin('styles.css');
+    const isProduction = env === "production";
 
-	return {
-		entry: ['babel-polyfill', './src/app.js', 'jquery'],
-		output: {
-			path: path.join(__dirname, 'dist'),
-			filename: 'bundle.js',
-		},
-		module: {		
-			rules: [
-				{
-					loader: 'babel-loader',
-					test: /\.js$/,
-					exclude: /node_modules/,
-				},
-				{
-					test: /\.s?css$/,
-					use: CSSExtract.extract({
-						use: [
-							{
-								loader: 'css-loader',
-								options: {
-									sourceMap: true,
-								},
-							},
-							{
-								loader: 'sass-loader',
-								options: {
-									sourceMap: true,
-								},
-							},
-						],
-					}),
-				},
-				{
-					test: /\.(jpg|png|gif|svg|pdf|ico)$/,
-					use: [
-						{
-							loader: 'file-loader',
-							options: {
-								name: '[path][name]-[hash:8].[ext]'
-							},
-						},
-					]
-				},
-			],
-		},
-		plugins: [CSSExtract, new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' })],
+    return {
+        entry: ["@babel/polyfill", "./src/app.js", "jquery"],
+        output: {
+            path: path.join(__dirname, "dist"),
+            filename: "bundle.js",
+            publicPath: "/",
+        },
+        optimization: {
+            minimizer: [new UglifyJsPlugin()],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"],
+                        },
+                    },
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        !isProduction ? "style-loader" : MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+                    loader: "file-loader?name=./public/*/[name].[ext]",
+                },
+            ],
+        },
+        plugins: [
+            new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" }),
+            new CleanWebpackPlugin(["dist"]),
+            new HtmlWebpackPlugin({
+                template: "./public/index.html",
+                favicon: "./public/images/favicon.png",
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css",
+            }),
+        ],
 
-		devtool: isProduction ? 'source-map' : 'inline-source-map',
-		devServer: {
-			contentBase: path.join(__dirname, 'public'),
-			historyApiFallback: true,
-			publicPath: '/dist/',
-			proxy: {
-				"/api/*": {
-				  target: "http://localhost:5000",
-				  secure: false,
-				  changeOrigin: true
-				},
-			  }
-		},
-	};
+        devtool: isProduction ? "source-map" : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true,
+            proxy: {
+                "/api/*": {
+                    target: "http://localhost:5000",
+                    secure: false,
+                    changeOrigin: true,
+                },
+            },
+        },
+    };
 };
