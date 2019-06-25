250adbbc0a1c64fa163e9b9d7be73b42a3d9d4da
James <james_gower@outlook.com>
2019-06-25 13:23:47 +0100

    Added ESlint support for typescript, updated types for tsx files

--- src/interfaces/TwitchAPI.ts
+++ src/interfaces/TwitchAPI.ts
@@ -1,63 +1,82 @@
 export default interface TwitchState {
-    users: string[];
-    onlineUserData: OnlineUser[];
-    offlineUserData: OfflineUser[];
-    isLoaded: boolean;
-    matureFilter: boolean;
-    show: string;
-    newStreamer: string;
-    error?: boolean;
-    usersToKeep?: string[];
+  users: string[];
+  onlineUserData: OnlineUser[];
+  offlineUserData: OfflineUser[];
+  isLoaded: boolean;
+  matureFilter: boolean;
+  show: string;
+  newStreamer: string;
+  error?: boolean;
+  usersToKeep?: string[];
 }
 
 export interface UserDataItemState {
-    showComponent: boolean;
-    userData: OnlineUser;
-    usersToKeep: string[];
-    desktop: boolean;
+  showComponent: boolean;
+  userData: OnlineUser;
+  usersToKeep: string[];
+  desktop: boolean;
+}
+
+export interface APICall {
+  result: object;
+  stream: {
+    game: string;
+    viewers: string;
+    average_fps: number;
+    created_at: string;
+    preview: {
+      medium: string;
+    };
+    channel: {
+      status: string;
+      logo: string;
+      mature: string;
+    };
+  };
+  game: object;
 }
 
 export interface UserDataItemProps {
-    name: string;
-    game?: string;
-    status?: string;
-    viewers?: string;
-    fps?: number;
-    image?: string;
-    online: boolean;
-    preview?: string;
-    mature?: string;
-    link: string;
-    removeUser: Function;
-    usersToKeep: string[];
-    matureFilter: boolean;
+  name: string;
+  game?: string;
+  status?: string;
+  viewers?: string;
+  fps?: number;
+  image?: string;
+  online: boolean;
+  preview?: string;
+  mature?: string;
+  link: string;
+  removeUser: Function;
+  usersToKeep: string[];
+  matureFilter: boolean;
 }
 
 export interface OnlineUser {
-    name: string;
-    game: string;
-    status: string;
-    viewers: string;
-    fps: number;
-    image: string;
-    online: boolean;
-    preview: string;
-    mature: string;
-    link: string;
-    lastSeen?: string;
-    lastGame?: string;
+  name: string;
+  game: string;
+  status: string;
+  viewers: string;
+  fps: number;
+  image: string;
+  online: boolean;
+  preview: string;
+  mature: string;
+  link: string;
+  lastSeen?: string;
+  lastGame?: string;
 }
 
 export interface SavedUser {
-    name: string;
-    lastGame: string;
-    image: string;
-    lastSeen: string;
-    link: string;
+  name: string;
+  lastGame: string;
+  image: string;
+  lastSeen: string;
+  link: string;
 }
 
 export interface OfflineUser {
-    name: string;
-    online: boolean;
-    link: string;
+  name: string;
+  online: boolean;
+  link: string;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,79 +6,79 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = env => {
-    const isProduction = env === "production";
+  const isProduction = env === "production";
 
-    return {
-        entry: ["@babel/polyfill", "./src/app.tsx", "jquery"],
-        resolve: {
-            extensions: [".ts", ".tsx", ".js"],
+  return {
+    entry: ["@babel/polyfill", "./src/app.tsx", "jquery"],
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "bundle.min.js",
+      publicPath: "/",
+    },
+    optimization: {
+      minimizer: [new UglifyJsPlugin()],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "awesome-typescript-loader",
         },
-        output: {
-            path: path.join(__dirname, "dist"),
-            filename: "bundle.min.js",
-            publicPath: "/",
-        },
-        optimization: {
-            minimizer: [new UglifyJsPlugin()],
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    loader: "awesome-typescript-loader",
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env", "@babel/preset-react"],
-                        },
-                    },
-                },
-                {
-                    test: /\.(sa|sc|c)ss$/,
-                    use: [
-                        !isProduction ? "style-loader" : MiniCssExtractPlugin.loader,
-                        "css-loader",
-                        "sass-loader",
-                    ],
-                },
-                {
-                    test: /\.(jpg|jpeg|png|gif|svg|pdf|ico)$/i,
-                    loader: "file-loader?name=[path][name].[ext]",
-                },
-                {
-                    test: /\.(mp3|wav|mpe?g)$/,
-                    loader: "file-loader?name=./public/media/[hash].[ext]",
-                },
-            ],
-        },
-        plugins: [
-            new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" }),
-            new CleanWebpackPlugin(["dist"]),
-            new HtmlWebpackPlugin({
-                template: "./public/index.html",
-                favicon: "./public/images/favicon.png",
-            }),
-            new MiniCssExtractPlugin({
-                filename: "[name].css",
-                chunkFilename: "[id].css",
-            }),
-        ],
-
-        devtool: isProduction ? "source-map" : "inline-source-map",
-        devServer: {
-            contentBase: path.join(__dirname, "public"),
-            historyApiFallback: true,
-            proxy: {
-                "/api/*": {
-                    target: "http://localhost:5000",
-                    secure: false,
-                    changeOrigin: true,
-                },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
             },
+          },
         },
-    };
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            !isProduction ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(jpg|jpeg|png|gif|svg|pdf|ico)$/i,
+          loader: "file-loader?name=[path][name].[ext]",
+        },
+        {
+          test: /\.(mp3|wav|mpe?g)$/,
+          loader: "file-loader?name=./public/media/[hash].[ext]",
+        },
+      ],
+    },
+    plugins: [
+      new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" }),
+      new CleanWebpackPlugin(["dist"]),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        favicon: "./public/images/favicon.png",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
+    ],
+
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+      proxy: {
+        "/api/*": {
+          target: "http://localhost:5000",
+          secure: false,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 };
