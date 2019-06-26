e6d43da1cdb58b0ff889432752b390a6c8d012a0
James <james_gower@outlook.com>
2019-06-26 14:25:41 +0100

    updated eslint, completed 95% of all responsive styling for TwitchAPI & UserDataItem components, seperated each components styling, and changed all styling to fit with styling conventions for scss

--- src/components/UserDataItem.tsx
+++ src/components/UserDataItem.tsx
@@ -6,7 +6,7 @@ const notFoundImage = require("../../public/images/twitch-not-found.jpg");
 
 /* 
 	TODO
-	[ ] Fix flexbox for online and offline
+	[x] Fix flexbox for online and offline
 */
 const initialState: UserDataItemState = {
   showComponent: true,
@@ -16,13 +16,18 @@ const initialState: UserDataItemState = {
 };
 
 class UserDataItem extends React.Component<UserDataItemProps, UserDataItemState> {
-  state = initialState;
+  public state = initialState;
 
   public componentDidMount(): void {
+    const { name } = this.props;
     const userDataJSON = localStorage.getItem(name);
     const userData = JSON.parse(userDataJSON);
     this.setState({ userData, desktop: window.innerWidth > 1000 });
     window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  public componentWillUnmount(): void {
+    window.removeEventListener("resize", this.updateWindowDimensions);
   }
 
   private updateWindowDimensions = (): void => {
@@ -104,35 +109,37 @@ class UserDataItem extends React.Component<UserDataItemProps, UserDataItemState>
       showComponent && (
         <div id={name} className="user__container animated fadeIn">
           <div className="user__details">
-            <div className={online ? "online-user" : "offline-user"}>
-              <div className="user-image">
+            <div className={online ? "user__online" : "user__offline"}>
+              <div className="user__imageContainer">
                 <img
                   alt={name}
                   src={image ? image : "/images/placeholder.png"}
-                  className="user-logo"
+                  className="user__image"
                 />
-                <h4 className="user-name">{name}</h4>
+                <h4 className="user__name">{name}</h4>
               </div>
-              <div className="user-info">
+              <div className="user__infoContainer">
                 {online ? <OnlineUserDetails /> : <OfflineUserDetails />}
-                <i
-                  role="button"
-                  onClick={this.onRemoveUser}
-                  onKeyPress={this.onRemoveUser}
-                  className="fa fa-times deleteBtn"
-                  tabIndex={0}
-                />
               </div>
             </div>
+            <i
+              role="button"
+              onClick={this.onRemoveUser}
+              onKeyPress={this.onRemoveUser}
+              className="fa fa-times user__deleteIcon"
+              tabIndex={0}
+            />
           </div>
           {desktop && (
             <div className="user__preview">
               <a href={link} target="_blank" rel="noopener noreferrer">
                 <img
-                  alt={`${name} preview`}
+                  alt={`${name} stream preview`}
                   src={preview ? preview : notFoundImage}
                   id={`${name}-img`}
-                  className={mature && matureFilter ? "preview-img-mature" : "preview-img"}
+                  className={
+                    mature && matureFilter ? "user__streamPreview--mature" : "user__streamPreview"
+                  }
                 />
               </a>
             </div>
