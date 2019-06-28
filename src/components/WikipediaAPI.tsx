f07aa61a8b9566416a4b16fdbd58be46939a6b73
James <james_gower@outlook.com>
2019-06-28 17:19:58 +0100

    added pure JS/TS event listeners on wikipediaAPI component rather than jQuery

--- src/components/WikipediaAPI.tsx
+++ src/components/WikipediaAPI.tsx
@@ -10,6 +10,46 @@ const initialState: WikiState = {
 
 class WikipediaAPI extends React.Component {
   public readonly state = initialState;
+
+  public componentDidMount(): void {
+    const button = document.getElementById("searchBtn");
+    const cancelBtn = document.getElementById("cancelBtn");
+    button.addEventListener("click", this.onSearchClick);
+    cancelBtn.addEventListener("click", this.onCancelClick);
+  }
+
+  public onSearchClick = (): void => {
+    const button = document.getElementById("searchBtn");
+    const input = document.getElementById("searchInput");
+    const cancelBtn = document.getElementById("cancelBtn");
+
+    button.classList.add("animated", "fadeOut");
+    input.classList.remove("hidden");
+    input.classList.add("animated", "zoomIn");
+    setTimeout(() => {
+      cancelBtn.classList.remove("hidden");
+      cancelBtn.classList.add("animated", "fadeIn");
+    }, 800);
+  };
+
+  public onCancelClick = (): void => {
+    const button = document.getElementById("searchBtn");
+    const input = document.getElementById("searchInput");
+    const cancelBtn = document.getElementById("cancelBtn");
+    cancelBtn.classList.remove("fadeIn");
+    cancelBtn.classList.add("fadeOut");
+
+    setTimeout(() => {
+      input.classList.remove("zoomIn");
+      input.classList.add("zoomOut");
+    }, 500);
+
+    setTimeout(() => {
+      button.classList.remove("fadeOut");
+      button.classList.add("fadeIn");
+    }, 800);
+  };
+
   public render(): JSX.Element {
     return (
       <div className="wiki__container">
@@ -22,13 +62,22 @@ class WikipediaAPI extends React.Component {
               </a>
             </p>
             <div className="wiki__searchContainer">
-              <input className="wiki__searchInput" type="search" placeholder="Search..." />
+              <input
+                className="wiki__searchInput hidden"
+                id="searchInput"
+                type="search"
+                placeholder="Search..."
+              />
               <img
                 alt="Search Button"
+                id="searchBtn"
                 className="wiki__searchBtn"
                 src="https://www.thesecu.com/wp-content/themes/secu/assets/images/Apps-Search-icon.png"
               />
-              <i className="fa fa-times-circle wiki__searchClear" />
+              <i
+                className="wiki__searchClear fa fa-times-circle wiki__searchClear hidden"
+                id="cancelBtn"
+              />
             </div>
             <p className="wiki__searchText">
               Or click the search button to search for a particular article.
