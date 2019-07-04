9d9273d740a4d7b63967c2ed2ba3b42f2a266ea5
James <james_gower@outlook.com>
2019-07-04 18:06:27 +0100

    completed HiddenNavBar and NavBar components (almost - closeBtn needs styling and positioned correctly) - navbar.scss mostly refactored

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom";
 import { Collapse, Navbar, NavbarToggler, Nav } from "reactstrap";
 import { NavBarProps, NavBarState } from "../interfaces/navBar";
 
+const closeIcon = require("../../public/images/closeBtn.svg");
+
 /**
  * TODO
  * [ ] Fix animation for clicking X button when close prop is passed
@@ -13,74 +15,100 @@ const logo = require("../../public/images/logo.png");
 
 const initialState = {
   isOpen: false,
+  hiddenNav: false,
 };
 
 class NavBar extends React.Component<NavBarProps, NavBarState> {
   public readonly state = initialState;
+  private navBarRef = React.createRef<HTMLDivElement>();
+  private closeBtnRef = React.createRef<HTMLImageElement>();
 
-  private toggle = (): void => {
+  public componentWillMount(): void {
+    const { closeNav } = this.props;
+    if (closeNav) this.setState({ hiddenNav: true });
+  }
+
+  private onNavToggle = (): void => {
     const { isOpen } = this.state;
     this.setState({
       isOpen: !isOpen,
     });
   };
 
+  private onCloseButton = (): void => {
+    const { closeNav } = this.props;
+    const nav = this.navBarRef.current;
+    const closeBtn = this.closeBtnRef.current;
+
+    nav.classList.add("animated", "slideOutUp");
+    closeBtn.classList.add("animated", "slideOutUp");
+    setTimeout((): void => {
+      closeNav();
+    }, 300);
+  };
+
   public render(): JSX.Element {
-    const { isOpen } = this.state;
-    const { close, onCloseNav } = this.props;
+    const { isOpen, hiddenNav } = this.state;
     return (
-      <div className="nav-container">
-        <Navbar className="navbar" color="faded" light expand="md">
-          <NavLink to="/">
-            <img src={logo} alt="JG Web Developer" className="logo" />
-          </NavLink>
-          <hr className="seperator" />
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavLink
-                className="nav-item nav-home"
-                exact
-                to="/"
-                activeClassName="active--home"
-              >
-                Home
-              </NavLink>
-              <NavLink
-                className="nav-item nav-portfolio"
-                to="/portfolio"
-                activeClassName="active--portfolio"
-              >
-                Portfolio
-              </NavLink>
-              <NavLink
-                className="nav-item nav-qual"
-                to="/skills"
-                activeClassName="active--qualifications"
-              >
-                Skills
-              </NavLink>
-              <NavLink
-                className="nav-item nav-me"
-                to="/contact-me"
-                activeClassName="active--qualifications"
-              >
-                About Me
-              </NavLink>
-            </Nav>
-          </Collapse>
-        </Navbar>
-        {close && (
+      <>
+        <div className="nav__container" ref={this.navBarRef}>
+          <Navbar color="faded" light expand="md">
+            <NavLink to="/">
+              <img src={logo} alt="JG Web Developer" className="logo" />
+            </NavLink>
+            <hr className="seperator" />
+            <NavbarToggler onClick={this.onNavToggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavLink
+                  className="nav__home"
+                  exact
+                  to="/"
+                  activeClassName="nav__home--active"
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  className="nav__portfolio"
+                  to="/portfolio"
+                  activeClassName="nav__portfolio--active"
+                >
+                  Portfolio
+                </NavLink>
+                <NavLink
+                  className="nav__skills"
+                  to="/skills"
+                  activeClassName="nav__skills--active"
+                >
+                  Skills
+                </NavLink>
+                <NavLink
+                  className="nav__about-me"
+                  to="/contact-me"
+                  activeClassName="nav__about-me--active"
+                >
+                  About Me
+                </NavLink>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+        {hiddenNav && (
           <span
             role="button"
             tabIndex={0}
             className="closeNav"
-            onClick={() => onCloseNav()}
+            onClick={this.onCloseButton}
           >
-            <i className="fa fa-times animated pulse infinite" />
+            <img
+              alt="Close Nav Button"
+              src={closeIcon}
+              className="animated pulse infinite"
+              ref={this.closeBtnRef}
+            />
           </span>
         )}
-      </div>
+      </>
     );
   }
 }
