7d31461ba7015d3989cf54f8686f21cc73dc99e4
James <james_gower@outlook.com>
2019-07-15 17:30:04 +0100

    removed a lot of unused dependencies, create timer for pomodoro component, needs refactoring as its very memory intensive atm.

--- src/pages/pomodoro/interfaces/pomodoro.i.ts
+++ src/pages/pomodoro/interfaces/pomodoro.i.ts
@@ -1,6 +1,9 @@
 interface PomodoroState {
   workTime: number;
   breakTime: number;
+  workTimerSeconds: number;
+  breakTimerSeconds: number;
+  currentTimer: string;
 }
 
 export { PomodoroState as default };
