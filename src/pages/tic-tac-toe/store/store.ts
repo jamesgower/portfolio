cc2c6348b804b01b87117cc0b994b0441f96b325
James <james_gower@outlook.com>
2019-07-15 17:30:04 +0100

    removed a lot of unused dependencies, create timer for pomodoro component, needs refactoring as its very memory intensive atm.

--- src/pages/tic-tac-toe/store/store.ts
+++ src/pages/tic-tac-toe/store/store.ts
@@ -1,13 +1,12 @@
 import thunk from "redux-thunk";
-import { createStore, combineReducers, applyMiddleware, compose, Store } from "redux";
-import { AppState } from "../interfaces/components.i";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import playerReducer from "../reducers/player.reducer";
 import boardReducer from "../reducers/board.reducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default (): Store<AppState> => {
-  const store: Store<AppState> = createStore<AppState>(
+export default (): any => {
+  const store = createStore(
     combineReducers({
       player: playerReducer,
       board: boardReducer,
