458fca608dca37026ec5f36aee03746f3a02475b
James <james_gower@outlook.com>
2019-07-17 11:06:01 +0100

    swapped environment variables to take priority in TwitchAPI components

--- src/pages/twitch-api/components/TwitchAPI.tsx
+++ src/pages/twitch-api/components/TwitchAPI.tsx
@@ -180,8 +180,8 @@ class TwitchAPI extends React.Component<{}, TwitchState> {
   public getData = async (name: string): Promise<void> => {
     const res = await fetch(`https://api.twitch.tv/kraken/streams/${name}`, {
       headers: {
-        "Client-ID": keys.twitch_client_id || process.env.twitch_client_id,
-        Authorization: keys.twitch_authorization || process.env.twitch_authorization,
+        "Client-ID": process.env.twitch_client_id || keys.twitch_client_id,
+        Authorization: process.env.twitch_authorization || keys.twitch_authorization,
       },
     });
     const result: APICall = await res.json();
